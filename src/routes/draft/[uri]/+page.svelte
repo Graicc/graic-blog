c51e852028f1acc3ef03c48e3c5b8bec8a4b1cde
Graicc <33105645+Graicc@users.noreply.github.com>
2025-08-16 03:34:22 -0400

    Add embeds for posts

--- src/routes/draft/[uri]/+page.svelte
+++ src/routes/draft/[uri]/+page.svelte
@@ -4,7 +4,11 @@
 
 <svelte:head>
 	<title>{data.title}</title>
-	<!-- <meta name="description" content="TODO" /> -->
+	<meta property="og:title" content={data.title} />
+	<meta property="og:description" content={data.subtitle} />
+	<meta property="og:type" content="article" />
+	<meta property="og:url" content={`https://graic.net/draft/${data.uri}`} />
+	<meta property="og:image" content="https://graic.net/favicon.png" />
 </svelte:head>
 
 <svelte:component this={data.content} />
